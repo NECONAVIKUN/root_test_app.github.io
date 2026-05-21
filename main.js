6628fec0197be29b247dbb68903c28cecb2fa54f
ねこナビくん <komukun7@gmail.com>
2026-05-21 13:13:10 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 function button_push (){
   const button = document.getElementById('m_button');
   if (button == "スタート！"){
-    document.getElementById('quiz').innerHTML = 
+    document.getElementById('<button').innerHTML = '<button id="<button">右が大</button>'
+    document.getElementById('>button').innerHTML = '<button id=">butoon">左が大</button>'
+    document.getElementById('m_button').innerHTML = '
   };
 };
